52d2c14160f0a4733116fcbde95444ffc5561705
Christopher Lee <clee@constantcontact.com>
2012-02-29 17:23:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Racktest::Application.config.secret_token = 'c7c3fd2c97e57bcbfb6e2c0a20a33582384c7ea997a034a3bf9feae52cf9051c8a59807c053c0b805a2189b740b6b0d6fd8e6ad7d4dbe945b9848cf2a852851a'
